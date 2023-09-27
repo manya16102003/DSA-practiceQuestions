f565ef52f340f8f65a24b43e4f314c78c313b191
Manya <123743851+manya16102003@users.noreply.github.com>
2023-09-28 04:00:59 +0530

    Updated Solution - GfG to GitHub

--- Medium/Painting the Fence/painting-the-fence.cpp
+++ Medium/Painting the Fence/painting-the-fence.cpp
@@ -7,21 +7,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    long long solve(int n,int k,vector<long long>&dp)
+    // long long solve(int n,int k,vector<int>&dp)
+    // {
+    //   long long mod=1000000007;
+    //     if(n==1) return k;
+    //     if(n==2) return k*k;
+    //     if(dp[n]!=-1) return dp[n];
+    //     long long a=solve(n-1,k,dp)*(k-1);
+    //     long long b=solve(n-2,k,dp)*(k-1);
+    //     dp[n]=(a+b)%mod;
+    //     return dp[n];
+    // }
+    long long solveb_t(int n,int k)
     {
-      long long mod=1000000007;
-        if(n==1) return k;
-        if(n==2) return k*k;
-        if(dp[n]!=-1) return dp[n];
-        long long a=solve(n-1,k,dp)*(k-1);
-        long long b=solve(n-2,k,dp)*(k-1);
-        dp[n]=(a+b)%mod;
+        long long mod=1000000007;
+        vector<long long>dp(n+1,0);
+        dp[1]=k;
+        dp[2]=k*k;
+        for(int i=3;i<=n;i++)
+        {
+             long long a=dp[i-1]*(k-1);
+             long long b=dp[i-2]*(k-1);
+             dp[i]=(a+b)%mod;
+        }
         return dp[n];
     }
     long long countWays(int n, int k){
         // code here
-        vector<long long>dp(n+1,-1);
-        return solve(n,k,dp);
+        // vector<long long>dp(n+1,-1);
+        return solveb_t(n,k);
     }
 };
 
